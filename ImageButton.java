bc7b2d4f2d4b2854a18d9706a9c22349a023bbb6
Pacy <40542052+pacy97@users.noreply.github.com>
2018-07-08 13:44:41 +0530

    Update ImageButton.java

--- ImageButton.java
+++ ImageButton.java
@@ -1,28 +1,32 @@
-
-package com.example.Pacasian.pacy;
-
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
- * Created by sumith on 16-10-2017.
+ * Created by sunith on 16-10-2017.
  */
 
-public class about extends AppCompatActivity {
-
-private Button button;
-
+public class ImageView extends AppCompatActivity  {
+    ImageButton imb;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about);
-        button=(TextView)findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.imagebutton);
+        imb = (ImageButton) findViewById(R.id.imageb);
+        imb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               //Add your operation here.......
+                  Toast.makeText(ImageButton.this, "You have Clicked the ImageButton", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
+
 }
